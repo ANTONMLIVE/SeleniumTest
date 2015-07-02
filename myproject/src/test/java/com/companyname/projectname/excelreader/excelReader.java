cfc9059cf8bf72f385856815b309a1703c94a12b
ANTONMLIVE <maheshanton@live.com>
2015-07-02 22:41:41 +0530

    Commited on 02 July 2015

--- myproject/src/test/java/com/companyname/projectname/excelreader/excelReader.java
+++ myproject/src/test/java/com/companyname/projectname/excelreader/excelReader.java
@@ -5,19 +5,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.apache.poi.xslf.usermodel.XSLFSheet;
-import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
-
 public class excelReader {
+
 	public static Object [][] data;
 	
-	public static boolean RunModeVerification (String TestCaseName) throws Exception{
-		
-		XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(new File ("")));
+	
+	//
+	public static boolean RunModeVerification(String TestCaseName) throws Exception
+	{
+		XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File (System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "input" + File.separator + "inputcontroller.xlsx")));
 		XSSFSheet TcSheetHome=workbook.getSheet("Home");
 		int TcrowHome=TcSheetHome.getLastRowNum();
 		for (int k=1;k<=TcrowHome;k++)
@@ -30,70 +30,75 @@ public class excelReader {
 			} 
 		}
 		return false;
-	}
-	
-	public static Object[][] selectSingleDataOrMulitiData(String TestCaseName) throws Exception
-	{
 		
-		XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File ("E:\\GitHub\\Selenium1\\framework001\\src\\test\\resources\\input\\inputcontroller.xlsx")));
-		XSSFSheet TcSheetHome=workbook.getSheet("Home");
-		int TcrowHome=TcSheetHome.getLastRowNum();
+	}
+	
+	
+	
+		//***Runmode data search
+		public static Object[][] selectSingleDataOrMulitiData(String TestCaseName) throws Exception
+		{
+			
+			XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File (System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "input" + File.separator + "inputcontroller.xlsx")));
+			XSSFSheet TcSheetHome=workbook.getSheet("Home");
+			int TcrowHome=TcSheetHome.getLastRowNum();
+			
+			for (int k=1;k<=TcrowHome;k++)
+			{
+				String TCNameHome=TcSheetHome.getRow(k).getCell(0).getStringCellValue();
+				if(TCNameHome.equals(TestCaseName))
+				{	
+				String RunForData=TcSheetHome.getRow(k).getCell(3).getStringCellValue();
+				if(RunForData.equals("Y"))
+				{
+					collectMultiData(TestCaseName);
+					break;
+				} else  
+				{
+					collectSingleData(TestCaseName);
+					break;
+				}
+				}
+			}
+			return data;
+		}
 		
-		for (int k=1;k<=TcrowHome;k++)
+		//***searching data sing run
+		public static Object[][] collectSingleData(String TestCaseName) throws Exception
 		{
-			String TCNameHome=TcSheetHome.getRow(k).getCell(0).getStringCellValue();
-			if(TCNameHome.equals(TestCaseName))
-			{	
-			String RunForData=TcSheetHome.getRow(k).getCell(3).getStringCellValue();
-			if(RunForData.equals("Y"))
+			XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File (System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "input" + File.separator +"inputcontroller.xlsx")));
+			XSSFSheet TcSheet=workbook.getSheet(TestCaseName);
+			int TcRow=TcSheet.getLastRowNum();
+			data=new Object[1][TcRow+1];
+			for (int i=0;i<=TcRow;i++)
 			{
-				collectMultiData(TestCaseName);
-				break;
-			} else  
-			{
-				collectSingleData(TestCaseName);
-				break;
+				Cell Cell=TcSheet.getRow(i).getCell(5);
+				TcSheet.getRow(i).getCell(5).setCellType(Cell.CELL_TYPE_STRING);
+				data[0][i]=TcSheet.getRow(i).getCell(5).getStringCellValue();
 			}
-			}
+			return data;
 		}
-		return data;
-	}
-	
-	//***searching data sing run
-	public static Object[][] collectSingleData(String TestCaseName) throws Exception
-	{
-		XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File ("E:\\GitHub\\Selenium1\\framework001\\src\\test\\resources\\input\\inputcontroller.xlsx")));
-		XSSFSheet TcSheet=workbook.getSheet(TestCaseName);
-		int TcRow=TcSheet.getLastRowNum();
-		data=new Object[1][TcRow+1];
-		for (int i=0;i<=TcRow;i++)
+		
+		//***Searching data for multi run
+		public static Object[][] collectMultiData(String TestCaseName) throws FileNotFoundException, IOException
 		{
-			XSSFCell Cell=TcSheet.getRow(i).getCell(5);
-			TcSheet.getRow(i).getCell(5).setCellType(Cell.CELL_TYPE_STRING);
-			data[0][i]=TcSheet.getRow(i).getCell(5).getStringCellValue();
-		}
-		return data;
-	}
-	
-	//***Searching data for multi run
-	public static Object[][] collectMultiData(String TestCaseName) throws FileNotFoundException, IOException
-	{
-		System.err.println("i am entered run for data");
-		XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File ("E:\\GitHub\\Selenium1\\framework001\\src\\test\\resources\\input\\multiData.xlsx")));
-		XSSFSheet TcSheet=workbook.getSheet(TestCaseName);
-		int TcRow=TcSheet.getLastRowNum();
-		int TcCol=TcSheet.getRow(1).getLastCellNum();
-		data=new Object[TcCol][TcRow+1];
-		for (int i=0;i<=TcRow;i++)
-		{
-			for (int j=0;j<TcCol;j++)
+			System.err.println("i am entered run for data");
+			XSSFWorkbook workbook=new XSSFWorkbook(new FileInputStream(new File (System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "input" + File.separator + "multiData.xlsx")));
+			XSSFSheet TcSheet=workbook.getSheet(TestCaseName);
+			int TcRow=TcSheet.getLastRowNum();
+			int TcCol=TcSheet.getRow(1).getLastCellNum();
+			data=new Object[TcCol][TcRow+1];
+			for (int i=0;i<=TcRow;i++)
 			{
-			XSSFCell Cell=TcSheet.getRow(i).getCell(j);
-			TcSheet.getRow(i).getCell(j).setCellType(Cell.CELL_TYPE_STRING);
-			data[j][i]=TcSheet.getRow(i).getCell(j).getStringCellValue();
+				for (int j=0;j<TcCol;j++)
+				{
+				Cell Cell=TcSheet.getRow(i).getCell(j);
+				TcSheet.getRow(i).getCell(j).setCellType(Cell.CELL_TYPE_STRING);
+				data[j][i]=TcSheet.getRow(i).getCell(j).getStringCellValue();
+				}
 			}
+			return data;
 		}
-		return data;
-	}
+
 
 }
